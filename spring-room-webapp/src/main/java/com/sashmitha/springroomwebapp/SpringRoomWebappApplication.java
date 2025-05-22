fa6dace55619e5ebfc7389113a7033ae3fb7c7b4
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2025-05-22 11:54:29 +0530

    Spring web and data jpa with H2 database

---
+++ spring-room-webapp/src/main/java/com/sashmitha/springroomwebapp/SpringRoomWebappApplication.java
@@ -0,0 +1,13 @@
+package com.sashmitha.springroomwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoomWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRoomWebappApplication.class, args);
+    }
+
+}
---
+++ spring-room-webapp/src/test/java/com/sashmitha/springroomwebapp/SpringRoomWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashmitha.springroomwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRoomWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
